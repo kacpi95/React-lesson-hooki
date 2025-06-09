9f9482e6d5a7e3caa6db72e0de25c090fa45f833
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-06-09 18:15:10 +0200

    Update project

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,18 @@
-import styles from "./App.module.css";
-import { Panel } from "./components/Panel/Panel";
+import { useState } from 'react';
+import styles from './App.module.css';
+import { Panel } from './components/Panel/Panel';
+import { Button } from './components/Button/Button';
 
 function App() {
-    return (
-        <main className={styles.main}>
-            <Panel />
-        </main>
-    );
+  const [isPanelShown, setIsPanelShown] = useState(true);
+  return (
+    <main className={styles.main}>
+      <Button onClick={() => setIsPanelShown((prev) => !prev)}>
+        {isPanelShown ? 'Schowaj panel' : 'Pokaż panel'}
+      </Button>
+      {isPanelShown && <Panel />}
+    </main>
+  );
 }
 
 export default App;
--- front-end/src/components/Panel/Panel.jsx
+++ front-end/src/components/Panel/Panel.jsx
@@ -1,36 +1,29 @@
-import { useState } from "react";
-import { Button } from "../Button/Button";
-import { List } from "../List/List";
-import styles from "./Panel.module.css";
+import { useState, useEffect } from 'react';
+import { List } from '../List/List';
+import styles from './Panel.module.css';
 
 export function Panel() {
-    const [data, setData] = useState([
-        {
-            id: 1,
-            word: "kot",
-            translation: "cat",
-            category: "noun",
-        },
-        {
-            id: 2,
-            word: "pies",
-            translation: "dog",
-            category: "noun",
-        },
-        {
-            id: 3,
-            word: "skakać",
-            translation: "jump",
-            category: "verb",
-        },
-    ]);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    return (
-        <>
-            <Button>Załaduje dane</Button>
-            <section className={styles.section}>
-                <List data={data}></List>
-            </section>
-        </>
-    );
+  useEffect(() => {
+    fetch('http://localhost:3000/words')
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <p>Ładowanie...</p>;
+  }
+
+  return (
+    <>
+      <section className={styles.section}>
+        <List data={data}></List>
+      </section>
+    </>
+  );
 }
