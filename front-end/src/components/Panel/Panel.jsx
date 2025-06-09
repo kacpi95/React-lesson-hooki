c732209c8fc890e5af2aed373b12312721f3de56
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-06-09 18:33:24 +0200

    task

--- front-end/src/components/Panel/Panel.jsx
+++ front-end/src/components/Panel/Panel.jsx
@@ -1,29 +1,51 @@
 import { useState, useEffect } from 'react';
-import { List } from '../List/List';
-import styles from './Panel.module.css';
+// import { List } from '../List/List';
+// import styles from './Panel.module.css';
 
 export function Panel() {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  // const [data, setData] = useState([]);
+  // const [isLoading, setIsLoading] = useState(true);
+
+  // useEffect(() => {
+  //   fetch('http://localhost:3000/words')
+  //     .then((res) => res.json())
+  //     .then((res) => {
+  //       setData(res);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
+  // if (isLoading) {
+  //   return <p>Ładowanie...</p>;
+  // }
+
+  // return (
+  //   <>
+  //     <section className={styles.section}>
+  //       <List data={data}></List>
+  //     </section>
+  //   </>
+  // );
+  const [isUsers, setIsUsers] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/words')
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then((res) => res.json())
       .then((res) => {
-        setData(res);
-        setIsLoading(false);
+        setIsUsers(res);
       });
   }, []);
 
-  if (isLoading) {
-    return <p>Ładowanie...</p>;
-  }
-
   return (
     <>
-      <section className={styles.section}>
-        <List data={data}></List>
-      </section>
+      <h1>Lista osób</h1>
+      <ul>
+        {isUsers.map((user) => (
+          <li key={user.id}>
+            {user.username}, {user.email}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
