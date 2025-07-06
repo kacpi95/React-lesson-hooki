107015385fa67fad88ad6e7d96b049319b6ffaca
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-07-06 19:01:33 +0200

    feat: fetch words with loading

--- front-end/src/components/Panel/Panel.jsx
+++ front-end/src/components/Panel/Panel.jsx
@@ -3,12 +3,22 @@ import styles from './Panel.module.css';
 import { Form } from '../Form/Form';
 import { ErrorMessage } from '../ErrorMessage/ErrorMessage';
 import { FilterButton } from '../FilterButton/FilterButton';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export function Panel() {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost3000/words')
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res);
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
     <>
       <ErrorMessage></ErrorMessage>
