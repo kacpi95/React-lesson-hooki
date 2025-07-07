f59da1696ea877ac25ab38543c36f1b464cc7791
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-07-07 13:00:00 +0200

    add: cleanup to useEfect

--- front-end/src/components/Panel/Panel.jsx
+++ front-end/src/components/Panel/Panel.jsx
@@ -12,13 +12,20 @@ export function Panel() {
   const [selectedCategory, setSelectedCategory] = useState(null);
 
   useEffect(() => {
+    let isCanceled = false;
     const params = selectedCategory ? `?category=${selectedCategory}` : '';
     fetch(`http://localhost:3000/words${params}`)
       .then((res) => res.json())
       .then((res) => {
-        setData(res);
-        setIsLoading(false);
+        if (!isCanceled) {
+          setData(res);
+          setIsLoading(false);
+        }
       });
+
+    return () => {
+      isCanceled = true;
+    };
   }, [selectedCategory]);
 
   function handleFormSubmit(formData) {
